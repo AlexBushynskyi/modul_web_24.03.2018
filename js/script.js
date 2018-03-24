1f04487c0982d53a70d1d41ffc346fa332593aa9
AlexBushynskyi <DangerWolfTrendOption@users.noreply.github.com>
2018-03-24 13:30:54 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var kvadrat =  document.getElementById('sp1');
+var triangle_k = document.getElementById('sp2');
+var triang_l_b =  document.getElementById('sp3');
+var triang_r_t = document.getElementById('sp4');
+
+kvadrat.onmouseover = function(event) {
+	
+	
+}
+
+kvadrat.onmouseout = function(event) {
+	
+}
